fdfffa7040430036f091ad7b9ee52f7ae4314276
Chad Scribner <chad.scribner@firespring.com>
2016-03-16 14:50:51 -0500

    Removing Bitcoin and Gold Miners

--- quote-only.gemspec
+++ quote-only.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'quote-only'
-  s.version     = '1.0.2'
-  s.date        = '2015-12-08'
+  s.version     = '1.0.3'
+  s.date        = '2016-03-16'
   s.summary     = 'Get only the quote for assets in your portfolio, without daily change.'
   s.description = 'Most finincial websites show the daily change of a financial asset, along with its quote.  This can be distracting to long-term investors attempting to "stay the course".  The purpose of this gem is to provide only the quote.  Markets and quotes are supported via money.com.  Treasuries are supported via bloomberg.com.  Precious Metals are supported using apmex.com.'
   s.authors     = ['cscribn']
--- test/test-quote-only.rb
+++ test/test-quote-only.rb
@@ -5,7 +5,7 @@ class QuoteOnlyTest < Minitest::Test
     def testCnnQuoteOnly(symbol, friendly_name, decimal_places)
         f = CnnQuoteOnly.new({ :symbol => symbol, :friendly_name => friendly_name, :decimal_places => decimal_places })
         puts f
-        assert f.quote.is_a? Float     
+        assert f.quote.is_a? Float
     end
 
     def test_
@@ -17,21 +17,20 @@ class QuoteOnlyTest < Minitest::Test
 
         f = ApmexSilverQuoteOnly.new({ :symbol => 'oz', :friendly_name => 'Silver', :decimal_places => 2 })
         puts f
-        assert f.quote.is_a? Float 
+        assert f.quote.is_a? Float
         puts
-        
+
         testCnnQuoteOnly('VTI', 'U.S. Stocks', 2)
         testCnnQuoteOnly('VT', 'Foreign Stocks', 2)
-        testCnnQuoteOnly('SGDM', 'Gold Miners', 2)
         puts
 
         testCnnQuoteOnly('VGLT', 'Long-Term Gov', 2)
-        testCnnQuoteOnly('BWZ', 'Foreign Cash', 2) 
         f = BloombergQuoteOnly.new({ :symbol => 'USGG10YR:IND', :friendly_name => '10 Year', :decimal_places => 2 })
         puts f
         assert f.quote.is_a? Float
-        testCnnQuoteOnly('XBT', 'Bitcoin', 2)
+        puts
 
+        testCnnQuoteOnly('BWZ', 'Foreign Cash', 2)
         puts
     end
 end
